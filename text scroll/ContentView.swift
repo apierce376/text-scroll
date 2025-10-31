5996bff6ad43d2ab573d32b692b954be27b837e4
Aaron Pierce <pierceaaron424@gmail.com>
2025-10-31 16:10:01 -0500

    Initial Commit

---
+++ text scroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  text scroll
+//
+//  Created by Aaron Pierce on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ text scroll/text_scrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_scrollApp.swift
+//  text scroll
+//
+//  Created by Aaron Pierce on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct text_scrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
