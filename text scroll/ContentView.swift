bbed2d5dc2d1dbb83ca931428ecd671c4f124f26
Aaron Pierce <pierceaaron424@gmail.com>
2025-11-04 17:28:01 -0600

    Example project

--- text scroll/ContentView.swift
+++ text scroll/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var start: String = "";
+    @State var end: String = "";
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                HStack {
+                    ScrollingText(old: 70, new: 105, animationDelay: 1);
+                    Text("%")
+                }
+                HStack {
+                    ScrollingText(old: 80, new: 92, animationDelay: 2);
+                    Text("hours")
+                }
+                HStack {
+                    ScrollingText(old: 97, new: 87, animationDelay: 3);
+                    Text("%")
+                }
+            }
         }
-        .padding()
+    }
+    
+    struct ScrollingText: View {
+        private var viewHeight: CGFloat;
+        private var old: Int;
+        private var new: Int;
+        private var animationDelay: Int;
+        private var animationDuration: Double;
+        
+        public init (viewHeight: CGFloat = 50, old: Int = 0, new: Int = 10, animationDelay: Int = 1, animationDuration: Double = 4.0) {
+            self.viewHeight = viewHeight;
+            self.old = old;
+            self.new = new;
+            self.animationDelay = animationDelay;
+            self.animationDuration = animationDuration;
+        }
+        
+        
+        var body: some View {
+            ScrollViewReader { proxy in
+                ScrollView {
+                    VStack {
+                        let lower = old > new ? new : old;
+                        let higher = old > new ? old : new;
+                        
+                        ForEach(lower...higher, id: \.self) { index in
+                            Text("\(index)")
+                                .id(index)
+                                .frame(width: .infinity, height: viewHeight)
+                                .font(Font.system(size:35))
+                                .fontWeight(Font.Weight.black)
+                        }
+                    }
+                }
+                .frame(height: viewHeight)
+                .onAppear() {
+                    proxy.scrollTo(old, anchor: .center)
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(animationDelay), execute: {
+                        withAnimation(.easeOut(duration: animationDuration)) {
+                            proxy.scrollTo(new, anchor: .center)
+                        }
+                    })
+                }
+            }
+        }
     }
 }
 
